8806d282e860a50fe19be693050a8ed2623fd5d4
Gerardo Soriano <gerardosoriano97@gmail.com>
2020-07-21 02:51:00 -0500

    Es7 block finished

---
+++ ecmascript6/src/es7/index.js
@@ -0,0 +1,15 @@
+//include function
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(7)) {
+  console.log('En efecto hay un 7');
+} else {
+  console.log('No hay ningún 7');
+}
+
+//pow
+let base = 4;
+let exponent = 4;
+let result = base ** exponent
+
+console.log(result);
